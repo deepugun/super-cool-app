844d6f172718c8e3c37053d57207c62e018d0ad0
TechReviews <TechReviews@TechReviewss-iMac.local>
2016-02-14 12:52:37 -0800

    added files for supercoolapp

--- supercoolapp/ViewController.swift
+++ supercoolapp/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Redlogo: UIImageView!
+    
+    @IBOutlet weak var Bluelogo: UIImageView!
+    
+    @IBOutlet weak var RedButton: UIButton!
+    
+    @IBOutlet weak var BlueButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func Blueaction(sender: AnyObject) {
+        Redlogo.hidden=true
+        Bluelogo.hidden=false
+    }
 
+    @IBAction func Redaction(sender: AnyObject) {
+        Bluelogo.hidden=true
+        Redlogo.hidden=false
+        
+    }
+    
 }
 
